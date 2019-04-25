2945eacacaedb01659e142895f1ddcc016f34f13
Alex Velasquez <jtinnesco@gmail.com>
2019-04-25 01:38:49 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,9 @@ def prime? (i)
  if i < 2
     return false 
  (2...i).each {|range_number| 
-  else i % range_number == 0}
+  elsif i % range_number == 0}
     return true
+  else 
+    false 
   end
 end
